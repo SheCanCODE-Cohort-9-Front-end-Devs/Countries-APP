7d7b6da8203982505c8641c3ef81eaba1142a78d
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-03-28 22:52:50 +0200

    first commit in new branch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <img src='https://media.istockphoto.com/id/183819034/photo/two-dices.jpg?s=612x612&w=0&k=20&c=sawA-Q6tAlmzm37EiFViPCr9AT3f-PnwCM8qXrQzR2w='></img>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
+    <nav className = "bg-black text-white py-3 flex gap-10 " >
+      <a href='https://tailwindcss.com/docs/guides/vite' > Tailwinf CSS with Vite</a>
+      <a href='https://countriesapi-eric.netlify.app/?page=1' > Download new version</a>
+      <a href='https://www.youtube.com/watch?v=2-crBg6wpp0&ab_channel=freeCodeCamp.org' > Github</a>
+      <a href='https://github.com/sylvie013' > Oragnisation</a>
+    </nav>
+  );
 }
 
 export default NavBar

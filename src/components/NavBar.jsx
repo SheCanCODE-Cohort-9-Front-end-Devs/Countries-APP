af926957448481f6e81525eace8d6ab5b295d9a1
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-04-01 22:57:52 +0200

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <nav className = "text-white pt-9 md:px-20 flex justify-between sm:px-5" >
       <a href='#'> 
-        <img className=' w-40 ' src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTLBBku8PBaMUd1PyD3C155FbzyeBPwEUjM8UanwsNNhnckAScO'></img>
+        <img className=' w-40 ' src='navbar.png'></img>
       </a>
       <a href='#' className=' hover:text-blue-500' > Countries </a>
     </nav>

49613853e648200e83142a4d4e0898883c40a437
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-04-01 23:05:22 +0200

    1

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -3,7 +3,7 @@ import { json } from 'react-router-dom'
 
 const Countries = () => {
   return (
-    <div className='m-24 flex justify-between h-44'>
+    <div className='md:m-24 flex justify-between md:flex-row sm:flex-col-reverse sm:m-6 h-44'>
       <p className=' text-xl'>View Countries</p>
       <p>Select continent </p>
       </div>

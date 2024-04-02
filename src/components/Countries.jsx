f4509993949acd8fa7e28920dc378898afc81d06
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-04-02 14:31:02 +0200

    new

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,7 +4,10 @@ import { json } from 'react-router-dom'
 const Countries = () => {
   return (
     <div className='md:m-24 flex justify-between md:flex-row sm:flex-col-reverse sm:m-6 h-44'>
-      <p className=' text-xl'>View Countries</p>
+      <div>
+      <p className=' text-2xl'>View Countries</p>
+      <p>page 1 of 5</p>
+      </div>
       <p>Select continent </p>
       </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import { useState } from 'react';
 
-const NavBar = () => { 
-  const [showMenu, setShowMenu] = useState(false);
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
-
+const NavBar = () => {
   return (
-    <div>
-    <nav className = "text-white pt-9 md:px-20 flex justify-between sm:px-5" >
+    <nav className = "text-white pt-9 md:px-20 flex justify-between items-center sm:px-5" >
       <a href='#'> 
         <img className=' w-40 ' src='navbar.png'></img>
       </a>
       <a href='#' className=' hover:text-blue-500' > Countries </a>
     </nav>
-    </div>
   );
 }
 

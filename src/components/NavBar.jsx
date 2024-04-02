e49af1bd8f751d3445156e675fcd54eec6f38ea9
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-04-02 09:16:02 +0200

    deployment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 
-const NavBar = () => {
+const NavBar = () => { 
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+
   return (
+    <div>
     <nav className = "text-white pt-9 md:px-20 flex justify-between sm:px-5" >
       <a href='#'> 
         <img className=' w-40 ' src='navbar.png'></img>
       </a>
       <a href='#' className=' hover:text-blue-500' > Countries </a>
     </nav>
+    </div>
   );
 }
 
